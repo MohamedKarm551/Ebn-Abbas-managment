93aa3ddd8ef8dcf948dfa40637d02133f701ea82
MohamedKarm551 <mohamdkarm551@gmail.com>
2025-04-14 03:36:06 +0200

    footer to hostinger

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -260,6 +260,20 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+        <footer class="bg-light py-3 mt-4 border-top">
+            <div class="container text-center">
+                <p class="mb-0">
+                    :تم التطوير بواسطة 
+                    <a href="https://github.com/MohamedKarm551" 
+                       target="_blank" 
+                       class="text-decoration-none">
+                        محمد كرم
+                        <i class="bi bi-github"></i>
+                    </a>
+                    ❤️
+                </p>
+            </div>
+        </footer>
         <style>
             .border-animation::before {
                 content: '';
